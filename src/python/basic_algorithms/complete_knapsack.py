5574cbef52ecc7087d500a7cbe9e89dbe93135f3
liadbiz <2712079998@qq.com>
2019-06-25 20:41:44 +0800

    add multiple_knapsack.py

--- src/python/basic_algorithms/complete_knapsack.py
+++ src/python/basic_algorithms/complete_knapsack.py
@@ -4,27 +4,30 @@
 def completeKnapsack(C, W, V):
     """complete knapsack problem solution
 
-    solve the 0-1 knapsack problem: given of N goods with weight and value for 
-    each good that are stored in W and V, select goods and put them into a 
+    solve the 0-1 knapsack problem: given of N items with weight and value for 
+    each item that are stored in W and V, select items and put them into a 
     knapsack with capacity C, find the maximum total value you can get.
 
-    diffrence with 0-1 knapsack problem: each good can be selected by any times
+    diffrence with 0-1 knapsack problem: each item can be selected by any times
 
     Args:
         C: capacity of knapsack
-        W: weights of goods
-        V: values of goods
+        W: weights of items
+        V: values of items
 
     Returns:
         maximum value you can get
     """
     assert len(W) != 0, "length of W should not be zero"
     assert len(W) == len(V), "lenth of W and length of V should be the same"
+
     N = len(W)
     dp = [0 for _ in range(C+1)]
+
     for i in range(N):
         for c in range(W[i], C + 1):
             dp[c] = max(dp[c], dp[c - W[i]] + V[i])
+
     return dp[-1]
 
 
--- src/python/basic_algorithms/multiple_knapsack.py
+++ src/python/basic_algorithms/multiple_knapsack.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def zeroOneKnapsack(C, W, V):
-    """0-1 knapsack problem solution
+def multipleKnapsack(C, W, V, M):
+    """multiple knapsack problem solution
 
     solve the 0-1 knapsack problem: given of N items with weight and value for 
     each item that are stored in W and V, select items and put them into a 
     knapsack with capacity C, find the maximum total value you can get.
 
+    diffrence with 0-1 knapsack problem: each item can be selected many times
+    that are stored in M.
+
     Args:
         C: capacity of knapsack
         W: weights of items
         V: values of items
+        M: maximum count for each item
 
     Returns:
         maximum value you can get
     """
+    # bind items to reduce time complexity
     assert len(W) != 0, "length of W should not be zero"
     assert len(W) == len(V), "lenth of W and length of V should be the same"
 
@@ -23,14 +28,21 @@ def zeroOneKnapsack(C, W, V):
     dp = [0 for _ in range(C+1)]
 
     for i in range(N):
-        for c in range(C, W[i] - 1, -1):
-            dp[c] = max(dp[c], dp[c - W[i]] + V[i])
+        m = 1
+        while m > 0:
+            w = W[i] * m
+            v = V[i] * m
+            for c in range(C, w - 1, -1):
+                dp[c] = max(dp[c], dp[c - w] + v)
+            M[i] -= m
+            m = min(m * 2, M[i])
 
     return dp[-1]
-
+    
 
 if __name__ == "__main__":
-    C = 15
+    C = 20
     W = [3,2,6,7,1,4,9,5]
     V = [6,3,5,8,3,1,6,9]
-    print(zeroOneKnapsack(C, W, V))
+    M = [3,5,1,9,3,5,6,8]
+    print(multipleKnapsack(C, W, V, M))
