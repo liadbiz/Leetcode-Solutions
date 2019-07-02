416fed6e441612e1ad82467d07ee1b5570386a94
liadbiz <2712079998@qq.com>
2019-07-02 15:12:54 +0800

    detail:
    * finish # 967

--- src/python/dynamic_programming/numbers_with_same_consecutive_difference.py
+++ src/python/dynamic_programming/numbers_with_same_consecutive_difference.py
@@ -21,4 +21,31 @@ Input: N = 2, K = 1
 Output: [10,12,21,23,32,34,43,45,54,56,65,67,76,78,87,89,98]
 
 """
+class Solution:
+    def numsSameConsecDiff(self, N, K):
+        """
+        :type N: int
+        :type K: int
+        :rtype: List[int]
+        """
+        res = set(range(10))
+        for i in range(N - 1):
+            new_res = set()
+            for n in res:
+                if n == 0:
+                    continue
+                last = n % 10
+                if last + K <= 9:
+                    new_res.add(10 * n  + last + K)
+                if last - K >= 0:
+                    new_res.add(10 * n  + last - K)
+            res = new_res
+        
+        return sorted(list(res))
+        
+if __name__ == "__main__":
+    N = 3
+    K = 7
+    result = [181, 292, 707, 818, 929]
+    assert Solution().numsSameConsecDiff(N, K) == result, "result is not correct"
 
